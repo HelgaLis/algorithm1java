c247967241ac21a5ba13a651d97176a53f922890
andy <andy@andy-netbook>
2015-10-08 08:14:29 +0300

    wheited quick union

--- ass1/QU.java
+++ ass1/QU.java
@@ -1,8 +1,12 @@
+import java.util.Arrays;
 
 public class QU {
 	private int[] id;
+	private int[] sz;
 	public QU(int size){
 		id = new int[size];
+		sz = new int[size];
+		Arrays.fill(sz, 1);
 		for(int i = 0; i<id.length; i++){
 			id[i] = i;
 			System.out.printf("%s ", id[i]);
@@ -30,10 +34,24 @@ public class QU {
 		}
 		return x;
 	}
+	public int treeSize(int x){
+		return sz[x];
+	}
+	public void connect(int p, int q){
+		if(sz[q]<sz[p]){
+			sz[p]+=sz[q];
+			sz[q]=0;
+			id[q]=id[p];
+		}
+		else{
+			sz[q]+=sz[p];
+			sz[p]=0;
+			id[p]=id[q];
+		}
+	}
 	public void checkConnection(int p, int q){
 		int rootP = isRoot(p), rootQ=isRoot(q);
-		if(rootP!=rootQ)
-			id[rootP]=id[rootQ];
+		connect(rootP, rootQ);
 	}
 	
 	public void union(int p, int q){
